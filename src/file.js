b340eea3e4c01101464e71d583446f3a635a080b
Mohammad Ravand <githubmohammadrepo@gmail.com>
2020-03-19 17:55:20 +0330

    completed

---
+++ src/file.js
@@ -0,0 +1,11 @@
+ // {
+            //     test: /\.(s[ac]ss|png|jpe?g|gif)$/i,
+            //     use: [
+            //         // Creates `style` nodes from JS strings
+            //         'style-loader',
+            //         // Translates CSS into CommonJS
+            //         'css-loader',
+            //         // Compiles Sass to CSS
+            //         'sass-loader',
+            //         'file-loader'
+            //     ],
